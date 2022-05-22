e46549a565e923049fe6c6904c60a895f7f71154
Tomytool <56263086+Tomytool@users.noreply.github.com>
2022-05-21 21:58:11 -0400

    Se deja componente App como contenedor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import ItemListContainer from './components/Itemlistcontainer/ItemListContainer';
+import NavBar from './components/Navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
+    <NavBar />
+    
       <header className="App-header">
       <ItemListContainer/>
       </header>
